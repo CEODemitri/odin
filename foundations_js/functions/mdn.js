5f728fbfcc50e771d83955081b512a44197ab38d
ceoDemitri <demichole@gmail.com>
2023-12-16 09:05:59 -0600

    functions practice

---
+++ foundations_js/functions/mdn.js
@@ -0,0 +1,19 @@
+// a piece of code that does one task inside a defined block
+
+const myText = "This is my string.";
+const newText = myText.replace("my", "our");
+console.log(newText);
+// the replace() string function
+
+const anArray = ["i", "love", "coding"];
+const anotherText = anArray.join(" ");
+console.log(anotherText);
+// join() function
+
+function printScreen(text) {
+  console.log(text);
+}
+// custom function + parameters
+
+printScreen("hey there");
+// invoking custom function
