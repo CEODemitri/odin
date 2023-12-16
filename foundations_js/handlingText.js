e0854ed8e6cccb9ff00db1c04a3c72b767f57fc9
ceoDemitri <demichole@gmail.com>
2023-12-16 06:56:57 -0600

    up to date

---
+++ foundations_js/handlingText.js
@@ -0,0 +1,10 @@
+const string = "I am the King.";
+console.log(string);
+
+const single = "Single quotes";
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
