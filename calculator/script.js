04176c053b981e5abf5c0e8b68e737cc4878ab1b
ceoDemitri <demichole@gmail.com>
2024-03-02 10:37:40 -0600

    finishing up this calculator

---
+++ calculator/script.js
@@ -0,0 +1,37 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+function subtractNumbers(a, b) {
+  return a + b;
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+function divideNumbers(a, b) {
+  return a / b;
+}
+
+// T E S T
+console.log(multiplyNumbers(5, 4));
+
+let number;
+let operator;
+let anotherNumber;
+
+function operate(operator, number, anotherNumber) {}
+
+const display = document.getElementById("display");
+
+const allClear = document.getElementById("oneOne");
+allClear.addEventListener("click", (event) => {});
+
+const twoOne = document.getElementById("twoOne");
+
+// T E S T . Works fine
+// twoOne.addEventListener("click", (event) => {
+//   console.log("7");
+//   document.getElementById("display").textContent = 7;
+// });
